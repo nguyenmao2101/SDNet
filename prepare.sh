eef864014845a30050922d7a3e878db80f647a88
nguyenmao2101 <maonguyen199873@gmail.com>
2020-07-31 09:55:29 +0700

    fix prepare s

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 mkdir coqa
 mkdir coqa/bert-base-uncased
-wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-vocab.txt -p coqa/bert-base-uncased/ 
-wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual.tar.gz -p coqa/bert-base-uncased/ 
+wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-vocab.txt -P coqa/bert-base-uncased/ 
+wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual.tar.gz -P coqa/bert-base-uncased/ 
 tar -xzf coqa/bert-base-uncased/bert-base-multilingual.tar.gz -C coqa/bert-base-uncased
